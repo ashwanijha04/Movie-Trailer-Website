04a6ffef13eed84fe719165f9254faa8adc02cab
Ashwani Jha <ashwanijha04@gmail.com>
2017-06-23 10:26:39 +0530

    Updated stypes using PEP-8

--- media.py
+++ media.py
@@ -1,35 +1,36 @@
-#importing the webbrowser module
+# importing the webbrowser module
 import webbrowser
 
-#Defining a movie class
+
+# Defining a movie class
 class Movie():
-	#A constant list of Valid Ratings
-	VALID_RATINGS = ["G", "PG", "PG-13", "R"]	
 
-	
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		""" The constructor is being defined which initialises various details of the movies to its instances. 
-			
-			Args:
-				1) Self: The first argument. This refers to the instance that uses it.
-				   For eg: self.story_line = story_line means that the story_line variable
-				   			will have a seperate copy for each instance that calls it.
+    # A constant list of Valid Ratings
+        VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-				2) movie_title: Contains a string of Movie Title.
+        def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+                """ The constructor is being defined which initialises various details of the movies to its instances.
 
-				3) movie_storyline: Contains a string of the Movie Storyline which is
-					a short summary of the movie that succintly describes it.
+                        Args:
+                                1) Self: The first argument. This refers to the instance that uses it.
+                                    For eg: self.story_line = story_line means that the story_line variable
+                                                will have a seperate copy for each instance that calls it.
 
-				4) poster_image: Contains a string of the link to the URL containing the image of the poster.
+                                2) movie_title: Contains a string of Movie Title.
 
-				5) trailer_youtube: Contains a string of the link to the Youtube URL of the trailer.
+                                3) movie_storyline: Contains a string of the Movie Storyline which is
+                                a short summary of the movie that succintly describes it.
 
-		"""
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+                                4) poster_image: Contains a string of the link to the URL containing the image of the poster.
 
-	def show_trailer(self):
-		""" This function opens a web browser with the given youtube URL. """
-		webbrowser.open(self.trailer_youtube_url)
+                                5) trailer_youtube: Contains a string of the link to the Youtube URL of the trailer.
+
+    """
+                self.title = movie_title
+                self.storyline = movie_storyline
+                self.poster_image_url = poster_image
+                self.trailer_youtube_url = trailer_youtube
+
+        def show_trailer(self):
+            """ This function opens a web browser with the given youtube URL. """
+            webbrowser.open(self.trailer_youtube_url)
